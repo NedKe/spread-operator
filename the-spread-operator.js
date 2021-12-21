5b10062af774c617f2a211aa0310d2f7aad09a61
Neda Keshavarzi <neda.keshavarz81@gmail.com>
2021-12-21 08:24:54 +0000

    No spread operator and copying an array

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -6,9 +6,20 @@
 // No spread operator
 let arr1 = [1, 2, 3];
 let arr2 = arr1; 
-console.log("secondarray:", arr2);
+arr2.push(4);
+console.log("second array:", arr2);
+console.log("First array", arr1);
+
 
 // Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+
+arr4.push(7);
+console.log("Third array:", arr3);
+console.log("Fourth array", arr4);
+
+
 
 // Copying an object
 
